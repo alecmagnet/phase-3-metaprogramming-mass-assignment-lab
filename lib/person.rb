c5635d82e45ca8e2fb4a868eab1ecdf81e39b557
Alec Magnet <amagnet@gmail.com>
2021-08-11 20:05:21 -0400

    commit tree b16a187992a8b96c5d0861eeec630a23f673bc09

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,30 @@
 class Person
-  # your code here
+  
+  # MOST ABSTRACT
+
+  # def initialize(attributes)
+  #   attributes.each do |key, value|
+  #     self.class.attr_accessor(key)
+  #     self.send("#{key}=", value)
+  #   end
+  # end
+
+  # MOST ABSTRACT -- PLUS IMPLICIT SELF IN LINE 17
+
+  def initialize(attributes)
+    attributes.each do |k, v|
+      self.class.attr_accessor(k)
+      send("#{k}=", v)
+    end
+  end 
+
+  # MORE COMPLICATED VERSION FOR PRACTICE
+
+  # attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size,:wrist_size, :glove_size, :pant_length, :pant_width
+
+  # def initialize(attributes)
+  #   attributes.each do |k, v|
+  #     send("#{k}=", v)
+  #   end
+  # end
 end
